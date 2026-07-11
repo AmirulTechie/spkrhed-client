20e310d41e40ef73e2e1c5ae237de2867272c788
Md. Amirul Islam <amirulislam.office28@gmail.com>
2026-07-12 01:20:15 +0600

    Add new VideoSection and ProblemSection components; include new images and update globals.css for styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import Hero from "@/components/homepage/hero/Hero";
 import MarqueeSection from "@/components/homepage/marquee/MarqueeSection";
 import StatsClientsSection from "@/components/homepage/stats-clients/StatsClientsSection";
+import VideoSection from "@/components/homepage/video/VideoSection";
+import ProblemSection from "@/components/homepage/problem/ProblemSection";
 
 export default function Home() {
   return (
@@ -8,6 +10,8 @@ export default function Home() {
       <Hero />
       <MarqueeSection />
       <StatsClientsSection />
+      <VideoSection />
+      <ProblemSection />
     </>
   );
 }
